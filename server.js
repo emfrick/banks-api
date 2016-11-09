a1f29e4c0aff8a42b51dd46ab187445f71a6ed63
Eric Frick <efrick@iq-inc.com>
2016-11-09 16:11:41 -0500

    app configuration

--- server.js
+++ server.js
@@ -1,16 +1,29 @@
 //
 // Module Imports
 //
-const express  = require('express');
-const app      = express();
-const jwt      = require('jsonwebtoken');
-const config   = require('./config');
+const express    = require('express');
+const app        = express();
+const jwt        = require('jsonwebtoken');
+const bodyParser = require('body-parser');
+const morgan     = require('morgan');
+const config     = require('./config');
 
 //
 // Setup the database
 //
 let Database = require('./database');
 new Database(`${config.db.host}:${config.db.port}/${config.db.name}`);
+
+//
+// Setup Body-Parser to grab JSON Post Requests and/or URL parameters
+//
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//
+// Morgan logs all requests
+//
+app.use(morgan('dev'));
 
 //
 // Initialize the routes
