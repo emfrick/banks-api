c3d14b4a9782ec0e8674ed62790ef2be0e41daa0
Eric Frick <emfrick@gmail.com>
2016-12-01 19:48:50 -0500

    Enable Google OAuth

--- routes/Auth.js
+++ routes/Auth.js
@@ -4,6 +4,7 @@
 const jwt        = require('jsonwebtoken');
 const expressJwt = require('express-jwt');
 const routes     = require('express').Router();
+const request    = require('request-promise');
 const config     = require('../config');
 
 //
@@ -39,11 +40,82 @@ routes.post('/', (req, res) => {
           let token = user.generateJwt();
           res.json({ token });
         });
-        
+
       })
       .catch(error => {
         res.json({ error });
       });
+});
+
+//
+// Handle Google OAuth 2.0
+//
+routes.post('/google', (req, res) => {
+
+  let access_token = req.body.access_token;
+
+  if (!access_token) {
+    res.status(400).json({
+      error: "Access Token is required"
+    });
+  }
+
+  // Validate the token against Google
+  let options = {
+    uri: 'https://www.googleapis.com/oauth2/v3/tokeninfo',
+    qs: {
+      access_token: access_token
+    },
+    json: true
+  };
+
+  request(options)
+        .then(validated => {
+          options.uri = 'https://www.googleapis.com/userinfo/v2/me';
+          return request(options);
+        })
+        .then(profile => {
+
+          // Return the result of this promise
+          return User.findOne({ email: profile.email})
+                     .then(user => {
+
+                       // If there's not a user, register the person
+                       if (!user) {
+
+                         // Create a new user model
+                         var user = new User();
+
+                         // Populate the fields
+                         user.email       = profile.email;
+                         user.first_name  = profile.given_name;
+                         user.last_name   = profile.family_name;
+                         user.signup_date = Date.now();
+                         user.is_active   = true;
+                         user.is_admin    = false;
+
+                         // Consider the user as logged in
+                         user.login_history.push(Date.now());
+                       }
+                       else {
+                         // Update the login time
+                         user.login_history.push(Date.now());
+                       }
+
+                       // Save the user
+                       return user.save();
+                     })
+        })
+        .then(user => {
+
+          let token = user.generateJwt();
+          res.json({ token });
+        })
+        .catch(error => {
+          res.status(500).json({
+            error
+          });
+        });
 });
 
 module.exports = routes;
